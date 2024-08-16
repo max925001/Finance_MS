04624a72914d469afeff3dde794e9e73a4f79928
max925001 <shivam969684@gmail.com>
2024-08-16 07:52:39 -0700

    Add link

--- client/src/helpers/axiosinstance.js
+++ client/src/helpers/axiosinstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const BASE_URL =  
-  "http://localhost:5001/api/v1"
-//  "https://finance-ms.onrender.com/api/v1"
+  // "http://localhost:5001/api/v1"
+ "https://finance-ms.onrender.com/api/v1"
 
 const axiosinstance = axios.create()
 axiosinstance.defaults.baseURL = BASE_URL
